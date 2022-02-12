f4390fa56f9e9f52360234f301048bc6110bddef
Mecit Tilim <mecittilim@gmail.com>
2022-02-12 16:18:51 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal :EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+        //join operasyonları yazılacak.
+    }
+}
